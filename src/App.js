36e6677ee72f9b9ab4505ce53499ac5f138e3e87
Fede Montagud <fedemontagudo@gmail.com>
2021-03-07 19:30:18 +0100

    commit inicial + cambio svg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './Bryozoa27.svg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mi primera aplicacion web con React, chavales
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.benditofutbol.com/files/article_main/uploads/2015/01/14/54b6caab9a8c1.jpg"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Pincha aqui para ver mi felicidad
         </a>
       </header>
     </div>
@@ -23,3 +23,5 @@ function App() {
 }
 
 export default App;
+
+
